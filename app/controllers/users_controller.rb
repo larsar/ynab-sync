f441ae65257c048b198b75d7a37b6ab484e2e0a5
Lars Preben Sørsdahl <lars@preben.no>
2018-10-06 20:40:04 +0200

    Stored ynab token in user table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,15 @@
 class UsersController < ApplicationController
   respond_to :html
+
+  def edit_ynab_access_token
+    @user = current_user
+  end
+
+  def update_access_token
+    current_user.ynab_access_token = params[:user].permit(:ynab_access_token)[:ynab_access_token]
+    current_user.save!
+    
+    redirect_to profile_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   authenticated :user do
     get 'profile', to: 'users#show'
 
+    get 'ynab', to: 'users#edit_ynab_access_token'
+    post 'ynab', to: 'users#update_access_token'
+
     #match 'services/:type/new', to: 'services#new', :via => [:get], as: 'new_service'
 
     resources :services do
