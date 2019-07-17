297984c6ca56aa594a635db6a4b1ff028d926ea2
Lars Preben Sørsdahl <lars@preben.no>
2019-07-17 10:54:42 +0200

    URL changes for Sbanken

--- app/lib/sbanken_api.rb
+++ app/lib/sbanken_api.rb
@@ -1,18 +1,18 @@
 class SbankenAPI
   def self.get_accounts(access_token, source_id, nin)
-    Rails.cache.fetch(['accounts', Sbanken.name, source_id], expires_in: 10.minutes) do
-      SbankenAPI.http_get('https://api.sbanken.no/bank/api/v1/Accounts', access_token, nin)['items']
+    Rails.cache.fetch(['accounts', Sbanken.name, source_id], expires_in: 1.minute) do
+      SbankenAPI.http_get('https://api.sbanken.no/exec.bank/api/v1/Accounts', access_token, nin)['items']
     end
   end
 
   def self.get_transactions(ext_account_id, access_token, source_id, nin)
-    Rails.cache.fetch(['transaction', Sbanken.name, ext_account_id, source_id], expires_in: 10.minutes) do
-      SbankenAPI.http_get("https://api.sbanken.no/bank/api/v1/Transactions/#{ext_account_id}?startDate=#{(Time.now - 2.weeks).strftime("%Y-%m-%d")}", access_token, nin)['items']
+    Rails.cache.fetch(['transaction', Sbanken.name, ext_account_id, source_id], expires_in: 1.minute) do
+      SbankenAPI.http_get("https://api.sbanken.no/exec.bank/api/v1/Transactions/#{ext_account_id}?startDate=#{(Time.now - 2.weeks).strftime("%Y-%m-%d")}", access_token, nin)['items']
     end
   end
 
   def self.get_access_token(client_id, client_secret, source_id)
-    Rails.cache.fetch(['access_token', Sbanken.name, source_id], expires_in: 3000.seconds) do
+    Rails.cache.fetch(['access_token', Sbanken.name, source_id], expires_in: 900.seconds) do
       url = URI.parse('https://auth.sbanken.no/identityserver/connect/token')
       http = Net::HTTP.new(url.host, url.port)
       req = Net::HTTP::Post.new(url.path, initheader = {
